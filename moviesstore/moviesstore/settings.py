2d08841607e342f0c4a2fc362d92f4d10f70482b
Braden Jones <98116111+TheRealBKJ@users.noreply.github.com>
2025-02-04 18:07:08 -0500

    fixes

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'home',
     'movies',
 ]
-
+#i am cracked
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
