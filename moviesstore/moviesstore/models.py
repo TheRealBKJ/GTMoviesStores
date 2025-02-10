c28979621c7ee7d0340c7e876ba106d99d810d8c
Dobromir <127112391+dobromiriiliev@users.noreply.github.com>
2025-02-10 16:29:15 -0500

    fixed indentation error

--- moviesstore/moviesstore/models.py
+++ moviesstore/moviesstore/models.py
@@ -20,3 +20,12 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.title}"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username} - {self.movie.title}"
