1b6c0a0254a27e8f8b8460c9713e899f2edf334b
Dobromir <127112391+dobromiriiliev@users.noreply.github.com>
2025-02-10 16:41:44 -0500

    finished implementing order and item models

--- moviesstore/movies/models.py
+++ moviesstore/movies/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db import models
 
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
@@ -8,14 +7,16 @@ class Movie(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
-def __str__(self):
-        return str(self.id) + ' - ' + self.name
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True)
-    movie = models.ForeignKey(Movie,
-        on_delete=models.CASCADE)
-    user = models.ForeignKey(User,
-        on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Review {self.id} by {self.user.username} on {self.movie.name}"
